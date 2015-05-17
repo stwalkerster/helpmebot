78be55e1c490ae83998da4365dece646e7d4a76b
Simon Walker <github@stwalkerster.co.uk>
2015-05-17 01:15:26 +0100

    rename file

--- src/Helpmebot/Helpmebot6.cs
+++ src/Helpmebot/Helpmebot6.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Helpmebot.cs" company="Helpmebot Development Team">
+// <copyright file="Helpmebot6.cs" company="Helpmebot Development Team">
 //   Helpmebot is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
 //   the Free Software Foundation, either version 3 of the License, or
